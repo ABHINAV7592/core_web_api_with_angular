ca8dcba2b3840cb5f1533e98d215e7e5be740fae
ABHINAV7592 <abhinavchandran77@gmail.com>
2025-12-16 14:18:22 +0530

    Updated

---
+++ core_web_api_with_angular/Controllers/user_managementController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using core_web_api_with_angular.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace core_web_api_with_angular.Controllers
+{
+    [Route("user_management")]
+    [ApiController]
+    public class user_managementController : ControllerBase
+    {
+        private readonly string _uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        public user_managementController()
+        {
+            if (!Directory.Exists(_uploadDir))
+            {
+                Directory.CreateDirectory(_uploadDir);
+            }
+        }
+        User dbobj = new User();
+        // GET: api/<user_management>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<user_management>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<user_management>
+        [HttpPost]
+        [Route("inserttab")]
+        public async Task<IActionResult> Post([FromForm] usercreateDTO createdto)
+        {
+            User usercls = new User();
+            if(createdto.path==null || createdto.path.Length == 0)
+            {
+                return BadRequest("No File Uploaded");
+            }
+            if (!createdto.path.ContentType.StartsWith("image/"))
+            {
+                return BadRequest("only Image Files Are Allowed");
+            }
+            var filepath = Path.Combine(_uploadDir, createdto.path.FileName);
+            using(var stream =new FileStream(filepath, FileMode.Create))
+            {
+                await createdto.path.CopyToAsync(stream);
+            }
+            usercls.name = createdto.name;
+            usercls.age= createdto.age;
+            usercls.address = createdto.address;
+            usercls.email = createdto.email;
+            usercls.photo = createdto.path.FileName;
+            usercls.username = createdto.username;
+            usercls.password = createdto.password;
+        }
+
+        // PUT api/<user_management>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<user_management>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- core_web_api_with_angular/Model/UserDB.cs
+++ core_web_api_with_angular/Model/UserDB.cs
@@ -5,9 +5,23 @@ namespace core_web_api_with_angular.Model
 {
     public class UserDB
     {
-        public UserDB() 
+        SqlConnection con = new SqlConnection(@"server=LAPTOP-USLUAF59\SQLEXPRESS;database=core_angular;integrated security=true");
+
+        public string insertdb(User clsobj)
         {
-            SqlConnection con = new SqlConnection(@"server=LAPTOP-USLUAF59\SQLEXPRESS;database=core_angular;integrated security=true");
+            SqlCommand cmd = new SqlCommand("sp_insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@na", clsobj.name);
+            cmd.Parameters.AddWithValue("@ag", clsobj.age);
+            cmd.Parameters.AddWithValue("@addr", clsobj.address);
+            cmd.Parameters.AddWithValue("@email", clsobj.email);
+            cmd.Parameters.AddWithValue("@photo", clsobj.photo);
+            cmd.Parameters.AddWithValue("@una", clsobj.username);
+            cmd.Parameters.AddWithValue("@pw", clsobj.password);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ("Inserted Successfully");
         }
 
 
