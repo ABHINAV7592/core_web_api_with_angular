c40fa5c3497afc4a21d8fc2d37e95603cd33f6fa
ABHINAV7592 <abhinavchandran77@gmail.com>
2025-12-16 13:36:02 +0530

    Add project files.

---
+++ core_web_api_with_angular/Model/User.cs
@@ -0,0 +1,29 @@
+﻿namespace core_web_api_with_angular.Model
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string? name { get; set; }
+        public int age { get; set; }
+        public string? address { get; set; }
+        public string? email { get; set; }
+        public string? photo { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+    public class usercreateDTO
+    {
+        public string? name { get; set; }
+        public int age { get; set; }
+        public string? address { get; set; }
+        public string? email { get; set; }
+        public IFormFile? path { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+    public class loginDTO
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+}
---
+++ core_web_api_with_angular/Model/UserDB.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace core_web_api_with_angular.Model
+{
+    public class UserDB
+    {
+        public UserDB() 
+        {
+            SqlConnection con = new SqlConnection(@"server=LAPTOP-USLUAF59\SQLEXPRESS;database=core_angular;integrated security=true");
+        }
+
+
+    }
+}
